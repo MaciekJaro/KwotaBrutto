85bd7c3865ea0ecb62b065b107b2ba684ae49ed0
MaciekJaro <90253712+MaciekJaro@users.noreply.github.com>
2021-09-07 13:32:59 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	float kwotabrutto;
+	
+cout<< "Podaj kwote Brutto"<<endl;	
+cin>>kwotabrutto;
+cout<<" kwota podatku wynosi \t"<<kwotabrutto*0.19<<endl;
+cout<<"kwota netto wynosi \t" <<kwotabrutto - kwotabrutto*0.19<<endl;
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
